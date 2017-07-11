f2f4f8351118a0211f765dd3777bdb7c351aabc9
Carlos Eugenio <carlos.eugenio@usach.cl>
2017-07-10 23:36:53 -0300

    added data and plots

--- MATLAB/saveCVDataToFile.m
+++ MATLAB/saveCVDataToFile.m
@@ -15,17 +15,17 @@ for i = 1:length(filenames)
     
     fid = fopen([dirPath '\' filenames{i} '.txt'],'w');
     fprintf(fid, 'Voltaje\tCorriente\n');
-    fprintf(fid, 'V\tA\n');
-    fprintf(fid, 'Vf\t%s, SR:%0.0f mV/s\n',DATA.parent, DATA.(filenames{i}).settings.SCANRATE);
+%     fprintf(fid, 'V\tA\n');
+%     fprintf(fid, 'Vf\t%s, SR:%0.0f mV/s\n',DATA.parent, DATA.(filenames{i}).settings.SCANRATE);
     fprintf(fid, '%f\t%f\n', cat(1,DATA.(filenames{i}).data.Vf.curve3,DATA.(filenames{i}).data.Im.curve3));
     fclose(fid);
 end
 
 %save capacitance data
 fid = fopen([dirPath '\' 'capacitance.txt'],'w');
-fprintf(fid, 'Scan Rate\t Capacitancia\n');
-fprintf(fid, 'mV/s\t F\n');
-fprintf(fid, 'Vf\t%s, Vwindow %0.1f V\n',DATA.parent,  DATA.sample1.settings.VLIMIT2-DATA.sample1.settings.VLIMIT1);
+fprintf(fid, 'ScanRate\t Capacitancia\n');
+% fprintf(fid, 'mV/s\t F\n');
+% fprintf(fid, 'Vf\t%s, Vwindow %0.1f V\n',DATA.parent,  DATA.sample1.settings.VLIMIT2-DATA.sample1.settings.VLIMIT1);
 
 for i = 1:length(filenames)
     if strcmp(filenames{i}, 'parent')
